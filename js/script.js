1e3fb46855d921ccbbf605af6b4146a5341a16dd
Kris <example@gmail.com>
2021-11-27 13:17:34 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//console.log(1);
+///* jshint esversion: 6 */
+//можно добавить в каждый файл или создать файл 
+//Вам нужно включить es6 в опциях jshint с опцией esversion. Если у вас нет файла .jshintrc на верхнем уровне, создайте его и добавьте
+//{ "esversion": 6 }
+//jshint.options: {
+  // "esversion": 6,
+//}
+
+"use strict";
+
+const result=+prompt("Are you hare?", "18");
+console.log(result);
